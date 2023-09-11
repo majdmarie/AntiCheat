a564b94b2669b55eb1029edc0799c40d83b2c363
Basil Hijaz <basilhijaz@yahoo.com>
2023-09-11 17:30:20 +0300

    Added Report Page with a bit of Styling

--- src/ReportStudent.js
+++ src/ReportStudent.js
@@ -1,93 +1,81 @@
 import React, { useState } from "react";
-
 import Form from "react-bootstrap/Form";
-
 import Button from "react-bootstrap/Button";
-
-import Dropdown from "react-bootstrap/Dropdown"
+import "./ReportStudent.css"; // Import the CSS file
 
 export default function ReportStudent() {
-
   const [reportedSeatNumber, setReportedSeatNumber] = useState("");
-
-  const [examRoom, setExamRoom] = useState("");
-
+  const [examRoom, setExamRoom] = useState("a"); // Initialize with a default value
   const [description, setDescription] = useState("");
 
   function validateForm() {
-
-    return reportedSeatNumber.length > 0 && examRoom.length > 0 && description.length > 0;
-
+    return reportedSeatNumber.length > 0 && description.length > 0;
   }
 
   function handleSubmit(event) {
-
     event.preventDefault();
-
+    // Handle form submission here
   }
 
   return (
-
-    <div className="Login">
-
-      <Form onSubmit={handleSubmit}>
-
-        <Form.Group size="lg" controlId="email">
-
-          <Form.Label>Reported Seat Number</Form.Label>
-
-          <Form.Control
-
-            autoFocus
-
-            value={reportedSeatNumber}
-
-            onChange={(e) => setReportedSeatNumber(e.target.value)}
-
-          />
-
-        </Form.Group>
-
-        <div>
-            
-            <label>Exam Room</label>
-
-            <select>
-
-                <option value="a">a</option>
-
-                <option value="b">b</option>
-
-                <option value="c">c</option>
-
-            </select>
-
+    <div className="container">
+      <div className="left-part">
+        {/* Fill the entire height of the page with the left part */}
+        <h2>Welcome to the Color Palette</h2>
+        <div className="color-box"></div>
+      </div>
+      <div className="right-part">
+        {/* Center the right part vertically and horizontally */}
+        <div className="center-content">
+          <div className="form-container">
+            <Form onSubmit={handleSubmit}>
+              <h2 className="form-title">Report Student</h2>
+              <Form.Group size="lg" controlId="reportedSeatNumber">
+                <Form.Label className="form-label">
+                  Reported Seat Number
+                </Form.Label>
+                <Form.Control
+                  className="form-input"
+                  autoFocus
+                  value={reportedSeatNumber}
+                  onChange={(e) => setReportedSeatNumber(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group size="lg" controlId="examRoom">
+                <Form.Label className="form-label">Exam Room</Form.Label>
+                <select
+                  className="form-select"
+                  value={examRoom}
+                  onChange={(e) => setExamRoom(e.target.value)}
+                >
+                  <option value="a">a</option>
+                  <option value="b">b</option>
+                  <option value="c">c</option>
+                </select>
+              </Form.Group>
+              <Form.Group size="lg" controlId="description">
+                <Form.Label className="form-label">Description</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  rows={4}
+                  className="form-input"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                />
+              </Form.Group>
+              <Button
+                variant="primary"
+                size="lg"
+                type="submit"
+                disabled={!validateForm()}
+                className="form-button"
+              >
+                Submit
+              </Button>
+            </Form>
+          </div>
         </div>
-
-        <Form.Group size="lg" controlId="examRoom">
-
-          <Form.Label>description</Form.Label>
-
-          <Form.Control
-
-            value={description}
-
-            onChange={(e) => setDescription(e.target.value)}
-
-          />
-
-        </Form.Group>
-
-        <Button block size="lg" type="submit" disabled={!validateForm()}>
-
-          submit
-
-        </Button>
-
-      </Form>
-
+      </div>
     </div>
-
   );
-
 }
